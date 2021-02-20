7b6c7aaf4677c288ba40a547cceeaa26ebfd588e
kruserr <46799551+kruserr@users.noreply.github.com>
2021-02-20 09:41:17 +0000

    #25: added BoxCollider

---
+++ src/classes/BoxCollider/BoxCollider.ts
@@ -0,0 +1,30 @@
+export interface IBoxCollider
+{
+  x : number;
+  y : number;
+  width : number;
+  height : number;
+}
+
+export default class BoxCollider
+{
+  x: number
+  y: number
+  width: number
+  height: number
+
+  constructor();
+  constructor(obj: IBoxCollider);
+  constructor(obj?: any)
+  {
+    this.x = obj && obj.x || 0
+    this.y = obj && obj.y || 0
+    this.width = obj && obj.width || 0
+    this.height = obj && obj.height || 0;
+  }
+
+  collideCheck(other: BoxCollider) : boolean
+  {
+    return false;
+  }
+}
