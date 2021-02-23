c48828d4014b1b3c4e7813a9387186943023857d
kruserr <46799551+kruserr@users.noreply.github.com>
2021-02-23 19:01:34 +0000

    #25: update access modifiers

--- src/classes/BoxCollider/BoxCollider.ts
+++ src/classes/BoxCollider/BoxCollider.ts
@@ -8,11 +8,14 @@ export interface IBoxCollider
 
 export default class BoxCollider
 {
-  x: number
-  y: number
-  width: number
-  height: number
+  private x: number;
+  private y: number;
+  private width: number;
+  private height: number;
 
+  private xOpen = true;
+  private yOpen = true;
+  
   constructor();
   constructor(obj: IBoxCollider);
   constructor(obj?: any)
@@ -22,6 +25,38 @@ export default class BoxCollider
     this.width = obj && obj.width || 0;
     this.height = obj && obj.height || 0;
   }
+
+  setX(x: number)
+  {
+    if (this.xOpen)
+    {
+      this.xOpen = false;
+      
+      this.x += x;
+
+      this.xOpen = true;
+    }
+  }
+  getX() { return this.x; }
+
+  setY(y: number)
+  {
+    if (this.yOpen)
+    { 
+      this.yOpen = false;
+      
+      this.y += y;
+
+      this.yOpen = true;
+    }
+  }
+  getY() { return this.y; }
+  
+  setWidth(width: number) { this.width = width; }
+  getWidth() { return this.width; }
+  
+  setHeight(height: number) { this.height = height; }
+  getHeight() { return this.height; }
 
   collideCheck(other: BoxCollider)
   {
