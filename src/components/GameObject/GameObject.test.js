7fb65ad30108efd062a3ec47718c6f23e6c8dce2
kruserr <46799551+kruserr@users.noreply.github.com>
2021-02-28 22:56:23 +0000

    #25: remove GameObject

--- src/components/GameObject/GameObject.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-
-import GameObject from './GameObject';
-
-
-test('renders GameObject', () => {
-  render(<GameObject />);
-});
