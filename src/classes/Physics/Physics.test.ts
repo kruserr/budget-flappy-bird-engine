63bf0b0a1a3867c0dd97256ee3753a43617cf466
kruserr <46799551+kruserr@users.noreply.github.com>
2021-03-09 20:54:38 +0000

    fea|4: added physics test

--- src/classes/Physics/Physics.test.ts
+++ src/classes/Physics/Physics.test.ts
@@ -3,41 +3,65 @@ import Physics from './Physics';
 
 test('Physics.ts - constructor()', () => {
   let sut = new Physics(-10, 1);
-  expect(sut.getVelocity).toBe(0);
+  expect(sut.getVelocity()).toBe(0);
 });
 
-test('Physics.ts - AxisAlignedBoundingBox()', () => {
-  let objA = new Pos({
-    x: 0,
-    y: 0,
+describe('test collision', () => {
+  class DummyDomRect implements DOMRect
+  {
+    height: number;
+    width: number;
+    x: number;
+    y: number;
+    bottom: number;
+    left: number;
+    right: number;
+    top: number;
+    toJSON() {
+      throw new Error('Method not implemented.');
+    }
+  };
+
+  test('Physics.ts - AxisAlignedBoundingBox()', () => {
+    let objA = new DummyDomRect();
+    objA.x = 0;
+    objA.y = 0;
+    objA.width = 1;
+    objA.height = 1;
+
+    let objB = new DummyDomRect();
+    objB.x = 0;
+    objB.y = 0;
+    objB.width = 1;
+    objB.height = 1;
+
+    expect(Physics.AxisAlignedBoundingBox(objA, objA)).toBe(true);
+    expect(Physics.AxisAlignedBoundingBox(objB, objB)).toBe(true);
+    expect(Physics.AxisAlignedBoundingBox(objA, objB)).toBe(true);
+    expect(Physics.AxisAlignedBoundingBox(objB, objA)).toBe(true);
+    
+    objA.x = 2;
+    objA.y = 2;
+    expect(Physics.AxisAlignedBoundingBox(objA, objB)).toBe(false);
+    
+    objB.x = 2;
+    objB.y = 2;
+    expect(Physics.AxisAlignedBoundingBox(objA, objB)).toBe(true);
+
+    objA.width = 0;
+    objA.height = 0;
+    expect(Physics.AxisAlignedBoundingBox(objA, objB)).toBe(false);
+
+    objA.width = 1;
+    objA.height = 0;
+    expect(Physics.AxisAlignedBoundingBox(objA, objB)).toBe(false);
+
+    objA.width = 0;
+    objA.height = 1;
+    expect(Physics.AxisAlignedBoundingBox(objA, objB)).toBe(false);
+
+    objA.width = 1;
+    objA.height = 1;
+    expect(Physics.AxisAlignedBoundingBox(objA, objB)).toBe(true);
   });
-  let objB = new Pos({
-    x: 0,
-    y: 0,
-  });
-  expect(objA.collideCheck(objB)).toBe(true);
-  
-  objA.x = 2;
-  objA.y = 2;
-  expect(objA.collideCheck(objB)).toBe(false);
-  
-  objB.x = 2;
-  objB.y = 2;
-  expect(objA.collideCheck(objB)).toBe(true);
-
-  objA.width = 0;
-  objA.height = 0;
-  expect(objA.collideCheck(objB)).toBe(false);
-
-  objA.width = 1;
-  objA.height = 0;
-  expect(objA.collideCheck(objB)).toBe(false);
-
-  objA.width = 0;
-  objA.height = 1;
-  expect(objA.collideCheck(objB)).toBe(false);
-
-  objA.width = 1;
-  objA.height = 1;
-  expect(objA.collideCheck(objB)).toBe(true);
 });
