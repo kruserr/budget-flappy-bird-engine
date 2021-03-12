7c3f6cb7d5758b275caf0168230319c9cec70e5e
kruserr <46799551+kruserr@users.noreply.github.com>
2021-03-12 11:05:40 +0000

    #53: remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import './index.css';
 
-import ctx from './classes/Data/Data';
 import Engine from './classes/Engine/Engine';
-import Pos from './classes/Pos/Pos';
-import Physics from './classes/Physics/Physics';
-import Audio from './classes/AudioSystem/AudioSystem';
 
 import Bird from './games/crappyBird/Bird';
 import Pipes from './games/crappyBird/Pipes';
