259072a7f78028a02e4d006304eec244c8bc4fcf
kruserr <46799551+kruserr@users.noreply.github.com>
2021-03-12 09:50:07 +0000

    #53: WIP: init

--- src/index.js
+++ src/index.js
@@ -144,57 +144,142 @@ function Hero(props)
 }
 engine.addObject(<Hero id={'Hero'} />);
 
-function Pipe(props)
+// function Pipe(props)
+// {
+//   const element = React.useRef(null);
+//   const [context, setContext] = React.useContext(ctx);
+
+//   const [collider, setCollider] = React.useState(
+//     new Pos({
+//       x: props?.data?.x,
+//       y: props?.data?.y,
+//     })
+//   );
+
+//   React.useEffect(() => {
+//     context[props?.id] = {
+//       'tag': 'obstacle'
+//     };
+//     setContext({...context});
+//   }, []);
+
+//   React.useEffect(() => {
+//     function move()
+//     {
+//       if (collider.x > -108.91)
+//       {
+//         collider.x += -6;
+//       }
+//       else
+//       {
+//         collider.x += 6*600;
+//       }
+
+//       setCollider(new Pos({...collider}));
+//     };
+
+//     engine.requestAnimationFrame(move);
+//   }, [collider]);
+
+//   let rotation = 0;
+//   if (props?.data?.rotation != null)
+//   {
+//     rotation = props?.data?.rotation;
+//   }
+
+//   const styleRoot = {
+//     position: `fixed`,
+//     willChange: `transform`,
+//     transform: `translate3d(${collider.x}px, ${collider.y}px, 0) rotate(${rotation}deg)`,
+//   };
+
+//   return (
+//     <svg ref={element} id={props?.id} style={styleRoot} height="800px" version="1.1" viewBox="0 0 70 514.19" xmlns="http://www.w3.org/2000/svg">
+//       <g transform="translate(-65 8.2278)" fill="#999" stroke="#000" strokeLinejoin="round">
+//         <rect transform="scale(1,-1)" x="70.798" y="-505.16" width="58.404" height="478.4" rx="0" ry="0" imageRendering="auto" strokeWidth="1.5964" style={{mixBlendMode: 'normal'}}/>
+//         <rect transform="scale(1,-1)" x="65.694" y="-26.078" width="68.612" height="33.612" ry="5.2732" strokeWidth="1.3882" style={{mixBlendMode: 'normal'}}/>
+//       </g>
+//     </svg>
+//   );
+// }
+
+// function PipeSet(props)
+// {
+//   function getRandomInt(min, max)
+//   {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min) + min);
+//   }
+
+//   const offset = 1000;
+//   const randomInt = getRandomInt(400, 800);
+
+//   const styleRoot = {
+
+//   };
+
+//   return (
+//     <span style={styleRoot}>
+//       <Pipe id={`${props?.id}_up`} data={{x: (600 * props?.data?.i) + 600, y: randomInt}}/>
+//       <Pipe id={`${props?.id}_down`} data={{x: ((600 * props?.data?.i) + 600), y: randomInt - offset, rotation: 180}}/>
+//     </span>
+//   );
+// }
+
+
+function Pipe({id, pos, rotation})
 {
-  const element = React.useRef(null);
-  const [context, setContext] = React.useContext(ctx);
+  // const element = React.useRef(null);
+  // const [context, setContext] = React.useContext(ctx);
 
-  const [collider, setCollider] = React.useState(
-    new Pos({
-      x: props?.data?.x,
-      y: props?.data?.y,
-    })
-  );
+  // const [collider, setCollider] = React.useState(
+  //   new Pos({
+  //     x: props?.data?.x,
+  //     y: props?.data?.y,
+  //   })
+  // );
 
-  React.useEffect(() => {
-    context[props?.id] = {
-      'tag': 'obstacle'
-    };
-    setContext({...context});
-  }, []);
+  // React.useEffect(() => {
+  //   function move()
+  //   {
+  //     if (collider.x > -108.91)
+  //     {
+  //       collider.x += -6;
+  //     }
+  //     else
+  //     {
+  //       collider.x += 6*600;
+  //     }
 
-  React.useEffect(() => {
-    function move()
-    {
-      if (collider.x > -108.91)
-      {
-        collider.x += -6;
-      }
-      else
-      {
-        collider.x += 6*600;
-      }
+  //     setCollider(new Pos({...collider}));
+  //     context[props?.id] = {
+  //       'collider': element?.current?.getBoundingClientRect(),
+  //       'tag': 'obstacle'
+  //     };
+  //     setContext({...context});
+  //   };
 
-      setCollider(new Pos({...collider}));
-    };
+  //   engine.requestAnimationFrame(move);
+  // }, [collider]);
 
-    engine.requestAnimationFrame(move);
-  }, [collider]);
-
-  let rotation = 0;
-  if (props?.data?.rotation != null)
+  if (rotation != null)
   {
-    rotation = props?.data?.rotation;
+    rotation = rotation;
+  }
+  else
+  {
+    rotation = 0;
   }
 
   const styleRoot = {
     position: `fixed`,
     willChange: `transform`,
-    transform: `translate3d(${collider.x}px, ${collider.y}px, 0) rotate(${rotation}deg)`,
+    transform: `translate3d(${pos?.x}px, ${pos?.y}px, 0) rotate(${rotation}deg)`,
   };
 
   return (
-    <svg ref={element} id={props?.id} style={styleRoot} height="800px" version="1.1" viewBox="0 0 70 514.19" xmlns="http://www.w3.org/2000/svg">
+    <svg id={id} style={styleRoot} height="800px" version="1.1" viewBox="0 0 70 514.19" xmlns="http://www.w3.org/2000/svg">
       <g transform="translate(-65 8.2278)" fill="#999" stroke="#000" strokeLinejoin="round">
         <rect transform="scale(1,-1)" x="70.798" y="-505.16" width="58.404" height="478.4" rx="0" ry="0" imageRendering="auto" strokeWidth="1.5964" style={{mixBlendMode: 'normal'}}/>
         <rect transform="scale(1,-1)" x="65.694" y="-26.078" width="68.612" height="33.612" ry="5.2732" strokeWidth="1.3882" style={{mixBlendMode: 'normal'}}/>
@@ -203,26 +288,44 @@ function Pipe(props)
   );
 }
 
-function PipeSet(props)
+function PipeSet({index, pos, spacing})
 {
-  function getRandomInt(min, max)
+  const [end, setEnd] = React.useState(false);
+
+  const startPos = React.useMemo(() => {
+    function getRandomInt(min, max)
+    {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+  
+    const offset = 1000;
+    const randomInt = getRandomInt(300, 700);
+
+    return {
+      'up': new Pos({x: (600 * index) + 600, y: randomInt}),
+      'down': new Pos({x: (600 * index) + 600, y: randomInt - offset}),
+    };
+  }, [end]);
+
+  let posDown = {...pos};
+  if (posDown?.y != null)
   {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+    posDown.y -= spacing;
   }
 
-  const offset = 1000;
-  const randomInt = getRandomInt(400, 800);
-
-  const styleRoot = {
-
-  };
-
   return (
-    <span style={styleRoot}>
-      <Pipe id={`${props?.id}_up`} data={{x: (600 * props?.data?.i) + 600, y: randomInt}}/>
-      <Pipe id={`${props?.id}_down`} data={{x: ((600 * props?.data?.i) + 600), y: randomInt - offset, rotation: 180}}/>
+    <span>
+      <Pipe
+        id={`${index}_up`}
+        pos={pos}
+      />
+      <Pipe
+        id={`${index}_down`}
+        pos={posDown}
+        rotation={180}
+      />
     </span>
   );
 }
